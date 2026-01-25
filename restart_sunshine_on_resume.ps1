6cfa439faa03a235df04603402f51af0ac4a8bad
blu <a-development+github@posteo.de>
2026-01-25 03:43:45 +0100

    Auto-commit: Changes detected on 2026-01-25 03:43:45

--- restart_sunshine_on_resume.ps1
+++ restart_sunshine_on_resume.ps1
@@ -1,19 +1,10 @@
 # Define task properties
 $TaskName = "RestartSunshineOnWake"
 $Action = New-ScheduledTaskAction -Execute "powershell.exe" -Argument "-WindowStyle Hidden -Command ""Start-Sleep -s 5; Restart-Service sunshine -Force"""
-$User = "SYSTEM" # Runs with highest possible privileges
+$User = "SYSTEM"
 
-# XML Trigger for Wake From Sleep (Event ID 1)
-$XmlTrigger = @"
-<QueryList>
-  <Query Id="0" Path="System">
-    <Select Path="System">*[System[Provider[@Name='Microsoft-Windows-Power-Troubleshooter'] and (EventID=1)]]</Select>
-  </Query>
-</QueryList>
-"@
-
-# Create Event Trigger
-$Trigger = New-ScheduledTaskTrigger -Xml $XmlTrigger
+# Create the trigger using EventTrigger class (works in older PowerShell versions)
+$Trigger = New-ScheduledTaskTrigger -EventLogName "System" -EventSource "Microsoft-Windows-Power-Troubleshooter" -EventID 1
 
 # Register the new scheduled task
 Register-ScheduledTask -TaskName $TaskName -Action $Action -Trigger $Trigger -User $User -Force
